07f1b923821cf77a8f9f032dc4d1ad2d17f08fbf
devaokta999-droid <devaokta999@gmail.com>
2025-11-11 15:06:48 +0800

    loginster

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -299,12 +299,12 @@
 
         if (passwordField.type === 'password') {
             passwordField.type = 'text';
-            toggleIcon.classList.remove('bi-eye');
-            toggleIcon.classList.add('bi-eye-slash');
-        } else {
-            passwordField.type = 'password';
             toggleIcon.classList.remove('bi-eye-slash');
             toggleIcon.classList.add('bi-eye');
+        } else {
+            passwordField.type = 'password';
+            toggleIcon.classList.remove('bi-eye');
+            toggleIcon.classList.add('bi-eye-slash');
         }
     }
 </script>
